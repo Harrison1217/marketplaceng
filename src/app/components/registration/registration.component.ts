91103d3992a4852ea46834372b161ee82d1e7294
ddcaldwell87 <ddcaldwell87@gmail.com>
2018-09-18 16:46:41 -0400

    Added register, token, login

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-registration',
@@ -11,7 +12,7 @@ export class RegistrationComponent implements OnInit {
 
   private _registerForm: FormGroup;
 
-  constructor(private _form: FormBuilder) { }
+  constructor(private _form: FormBuilder, private _authService: AuthService) { }
 
   ngOnInit() {
     this.createForm();
@@ -27,6 +28,9 @@ export class RegistrationComponent implements OnInit {
 
   onSubmit() {
     console.log(this._registerForm.value);
+    this._authService
+      .register(this._registerForm.value)
+      .subscribe( () => this._authService.login(this._registerForm.value));
   }
 
 }
