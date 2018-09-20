42f7a203a227701ed1eb5a168a5b453362fdd79a
unknown <hball1217@gmail.com>
2018-09-20 10:46:59 -0400

    Worked on Transaction Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ const routes = [
   { path: 'register', component: RegistrationComponent },
   { path: 'login', component: LoginComponent },
   { path: 'customers', component: CustomerIndexComponent },
+  { path: 'transactions', component: TransactionIndexComponent },
   { path: '**', component: RegistrationComponent }
 ];
 
