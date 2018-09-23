febd96e090cbc757a1f362c9d4180e7643b84580
ssjauhal <ssjauhal@gmail.com>
2018-09-23 14:39:32 -0400

    fixed conflict

--- src/app/models/Transaction.ts
+++ src/app/models/Transaction.ts
@@ -1,21 +1,13 @@
 export interface Transaction {
     TransactionId?: number;
     ProductId?: number;
-<<<<<<< HEAD
     //Product: Product;
-=======
-    // Product: Product;
->>>>>>> bd7c37190cf4c0e3a038906f571583ca02ca9042
     NumItems: number;
     ProductName: string;
     ProductQuntity: number;
     ProductUPC: number;
     ProductPrice: number;
     CustomerId?: number;
-<<<<<<< HEAD
-    //Customer: Customer;
-=======
     // Customer: Customer;
->>>>>>> bd7c37190cf4c0e3a038906f571583ca02ca9042
     TransactionDate: Date;
 }
