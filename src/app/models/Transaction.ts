24f22861751cc49ca958fd80f0464b3bd4eea5ef
unknown <hball1217@gmail.com>
2018-09-24 09:36:47 -0400

    Added import statments to Transaction.Ts for products and Customer

--- src/app/models/Transaction.ts
+++ src/app/models/Transaction.ts
@@ -1,13 +1,15 @@
+import { Customer } from '../models/Customer'
+import { Product } from '../models/Product'
 export interface Transaction {
     TransactionId?: number;
     ProductId?: number;
-    //Product: Product;
+    Product: Product;
     NumItems: number;
     ProductName: string;
     ProductQuntity: number;
     ProductUPC: number;
     ProductPrice: number;
     CustomerId?: number;
-    // Customer: Customer;
+    Customer: Customer;
     TransactionDate: Date;
 }
