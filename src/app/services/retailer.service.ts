45bfc50dc4ca1d72cab66f60c6025419aac327d8
Bailey Riker <baileyjamesriker@gmail.com>
2018-09-27 09:10:11 -0400

    doing retailer page icons

--- src/app/services/retailer.service.ts
+++ src/app/services/retailer.service.ts
@@ -12,12 +12,21 @@ export class RetailersService {
   constructor(private _http: HttpClient) {}
   
   getRetailers() {
-    return this._http.get(`${ApiUrl}/Retailer`, { headers: this.getHeaders() });
+    return this._http.get(`${ApiUrl}/retailer`, { headers: this.getHeaders() });
   }
   createRetailers(retailer: Retailer){
-    return this._http.post(`${ApiUrl}/Retailer`, retailer, {headers: this.getHeaders()});
+    return this._http.post(`${ApiUrl}/retailer`, retailer, {headers: this.getHeaders()});
+  }
+  getRetailer(id: string){
+    return this._http.get(`${ApiUrl}/retailer/${id}`, {headers: this.getHeaders()});
+  }
+  updateRetailer(retailer: Retailer){
+    return this._http.put(`${ApiUrl}/retailer`, retailer, {headers: this.getHeaders()});
   }
 
+  deleteRetailer(id: number){
+    return this._http.delete(`${ApiUrl}/retailer/${id}`, {headers:this.getHeaders()});
+  }
   getHeaders() {
     return new HttpHeaders().set('Authorization', `Bearer ${localStorage.getItem('id_token')}`);
   }
