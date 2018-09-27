1ad511a12f24c4f735dbcbf7abf457648c834f6f
ddcaldwell87 <ddcaldwell87@gmail.com>
2018-09-27 11:04:26 -0400

    url

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,7 +5,7 @@ import { Token } from '../models/Token';
 import { Router } from '@angular/router';
 import { Observable, Subject } from 'rxjs';
 
-const Api_Url = 'http://localhost:51668';
+const Api_Url = 'https://efamarketplacewebapi.azurewebsites.net';
 
 @Injectable({
   providedIn: 'root'
