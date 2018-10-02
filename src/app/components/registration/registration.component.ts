0912d8414a14ad5b6e8460077e9c01b777d93d1a
ssjauhal <ssjauhal@gmail.com>
2018-10-02 14:48:10 -0400

    update

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -12,7 +12,7 @@ import { Router } from '@angular/router';
 
 export class RegistrationComponent implements OnInit {
 
-  private _registerForm: FormGroup;
+  public _registerForm: FormGroup;
 
   selectedValue: string;
 
